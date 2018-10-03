b7d333a4dbc3d2b66f585a84aee0a0196f960dc1
Anderson.paterno <anderson_paterno@estudante.sc.senai.br>
2018-10-03 18:55:43 -0300

    Primeiro commit

---
+++ Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Primeira Edição Git");
+		System.out.println("Segunda Edição");
+		System.out.println("Visualizando Alterações no Repositórios ");
+		
+	}
+
+}
